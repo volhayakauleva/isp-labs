5316e65603acd17f3abfb1e82138f9f9963f802c
volhayakauleva <52138253+volhayakauleva@users.noreply.github.com>
2020-03-26 12:29:57 +0300

    th

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Person
+    {
+        protected static int personCounter;
+        protected string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+            personCounter++;
+        }
+
+        public static void peopleAmount()
+        {
+            Console.WriteLine("\nОбщее количество людей: " + personCounter);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Имя: " + Name);
+        }
+    }
+
+    class Student : Person
+    {
+        protected string University { get; set; }
+
+        public Student(string name, string university) : base(name)
+        {
+            University = university;
+        }
+        public new void Show()
+        {
+            Console.WriteLine("Имя: " + Name + "\nУниверситет: " + University);
+        }
+    }
+
+    class StudentWithSpecialty : Student
+    {
+        protected string Specialty { get; set; }
+
+        public StudentWithSpecialty(string name, string university, string specialty) : base(name, university)
+        {
+            Specialty = specialty;
+        }
+        public new void Show()
+        {
+            Console.WriteLine("Имя: " + Name + "\nУниверситет: " + University + "\nСпециальность: " + Specialty);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StudentWithSpecialty prog = new StudentWithSpecialty("Ваня", "БГУИР", "Программист");
+            StudentWithSpecialty math = new StudentWithSpecialty("Маша", "БГУИР", "Математик");
+            StudentWithSpecialty paint = new StudentWithSpecialty("Петя", "БГУ", "Художник");
+
+            prog.Show();
+            Person.peopleAmount();
+
+            Console.ReadKey();
+        }
+    }
+}
+
